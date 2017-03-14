83709a42edcfa544889dca12f47ff4e42b31d2fd
성준영 <wnsdud1861@gmail.com>
2017-03-14 22:20:53 +0900

    index 페이지 mysql 연결 / UI 변경

--- app.js
+++ app.js
@@ -1,14 +1,54 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static(__dirname + '/html'));
 app.use(express.static(__dirname + '/css'));
 app.use(express.static(__dirname + '/js'));
 
+app.use(bodyParser.urlencoded({extend: false}));
+app.use(bodyParser.json());
+
+var mysql = require('mysql');
+var databaseConfig = require('./config/database.json');
+var connection = mysql.createConnection({
+    host: databaseConfig.db_host,
+    user: databaseConfig.db_user,
+    password: databaseConfig.db_password,
+    database: databaseConfig.db_name
+});
+
 
 app.get('/', function (req, res) {
-  res.sendFile('index.html');
+    res.sendFile('index.html');
 });
+
+app.post('/', function (req, res) {
+
+    //TODO : 전화번호 형식 거르기
+
+    //TODO : 중복된 전화번호 거르기
+
+    // 등록시간
+    var date = new Date();
+    var formatDate = date.getFullYear() + ("0" + (date.getMonth() + 1)).slice(-2) + ("0" + date.getDate()).slice(-2) + ("0" + date.getHours() + 1 ).slice(-2) + ("0" + date.getMinutes()).slice(-2) + ("0" + date.getSeconds()).slice(-2);
+
+    // mysql 쿼리
+    connection.query('INSERT INTO hps (hp_1, hp_2, hp_3, hp_full, hp_regtime)' + ' VALUES (' + req.body.hp_input_1 + ',' + req.body.hp_input_2 + ',' + req.body.hp_input_3 + ',' + req.body.hp_input_1 + req.body.hp_input_2 + req.body.hp_input_3 + ',' + formatDate + ')', function (err, rows, fields) {
+        // 실패시
+        if (err) {
+            // TODO 실패 알림 / index 페이지 리다이렉트
+            console.log(err);
+            res.send('<script type="text/javascript">alert("데이터베이스 추가 중 에러가 발생했습니다.");</script>');
+            // res.sendFile();
+        }
+
+    });
+
+    // TODO 성공시 성공 알림 / index 페이지 리다이렉트
+    res.sendFile('html/index.html', {root: __dirname});
+});
+
 
 app.listen(3000, function () {
 });
